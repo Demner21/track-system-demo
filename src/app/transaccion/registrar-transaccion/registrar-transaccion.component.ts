3569fd29bbde3fda6b6a3105724865825f0cc23f
Demneru <18225600+Demner21@users.noreply.github.com>
2020-10-03 22:35:32 -0500

    componente de registrar transaccion creado

---
+++ src/app/transaccion/registrar-transaccion/registrar-transaccion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-transaccion',
+  templateUrl: './registrar-transaccion.component.html',
+  styleUrls: ['./registrar-transaccion.component.css']
+})
+export class RegistrarTransaccionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
