c4cb8328bd335e3c59d30c82c630d82b12246edd
Demneru <18225600+Demner21@users.noreply.github.com>
2020-10-03 22:39:44 -0500

    agregando componente trazabilidad

--- src/app/transaccion/transaccion.component.ts
+++ src/app/transaccion/transaccion.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-@Component({
-  selector:'app-transaccion',
-  templateUrl:'./transaccion.component.html'
-})
-export class TransaccionComponent{
 
+@Component({
+  selector: 'app-transaccion',
+  templateUrl: './transaccion.component.html'
+})
+export class TransaccionComponent {
+  nombreAplicacion = 'SIGEX';
+  idTransaccion = '012354785';
 }
