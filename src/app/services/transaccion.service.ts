57cb23f85ecda02451c32f0bb198cc9e18bf8d18
Demneru <18225600+Demner21@users.noreply.github.com>
2020-10-08 00:06:09 -0500

    correccion de transaccion valor

--- src/app/services/transaccion.service.ts
+++ src/app/services/transaccion.service.ts
@@ -2,7 +2,7 @@ import { Transaccion } from './../transaccion/transaccion.model';
 
 export class TransaccionService{
   listaTransacciones:Transaccion[]=[
-    new Transaccion('SISACT','VENTA','123')
+    new Transaccion('SISACT','VENTA','Cambio Titularidad')
   ];
 
   registrarTransaccion(transaccionData:Transaccion){
