fd968ba26d9682cfd17b5068d8be8fe16969ead6
Demneru <18225600+Demner21@users.noreply.github.com>
2020-10-13 11:52:01 -0500

    componente de insumos completado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { ListarTransaccionesComponent } from './transaccion/listar-transacciones/listar-transacciones.component';
 import { RegistrarTransaccionComponent } from './transaccion/registrar-transaccion/registrar-transaccion.component';
 import { ListarTrazabilidadesComponent } from './trazabilidad/listar-trazabilidades/listar-trazabilidades.component';
+import { RegistrarInsumosComponent } from './trazabilidad/registrar-trazabilidad/registrar-insumos/registrar-insumos.component';
 import { RegistrarTrazabilidadComponent } from './trazabilidad/registrar-trazabilidad/registrar-trazabilidad.component';
 
 const routes: Routes = [
   { path:'registarTansaccion', component:RegistrarTransaccionComponent},
   { path:'listarTransacciones', component: ListarTransaccionesComponent},
   { path:'registrarTrazabilidad', component:RegistrarTrazabilidadComponent},
-  { path:'listarTrazabilidades', component:ListarTrazabilidadesComponent}
+  { path:'listarTrazabilidades', component:ListarTrazabilidadesComponent},
+  { path:'registarInsumo', component:RegistrarInsumosComponent}
 ];
 
 @NgModule({
