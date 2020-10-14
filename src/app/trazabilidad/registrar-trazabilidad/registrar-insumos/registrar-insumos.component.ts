da40a3a08bde58291c0b2d3e444c93b845b74904
Demneru <18225600+Demner21@users.noreply.github.com>
2020-10-13 22:00:31 -0500

    correccion usando forms de angular

--- src/app/trazabilidad/registrar-trazabilidad/registrar-insumos/registrar-insumos.component.ts
+++ src/app/trazabilidad/registrar-trazabilidad/registrar-insumos/registrar-insumos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { SupplierDataService } from 'src/app/services/supplier.data.service';
 import { TransaccionService } from 'src/app/services/transaccion.service';
 
@@ -8,21 +9,38 @@ import { TransaccionService } from 'src/app/services/transaccion.service';
   styleUrls: ['./registrar-insumos.component.css']
 })
 export class RegistrarInsumosComponent implements OnInit {
-  nombreAplicacion:string='';
-  codigoProyecto:string='';
+  @ViewChild('form', { static: false }) signupForm: NgForm;
+
+  insumo={
+    nombreAplicacion:'',
+    codigoProyecto:'',
+    transaccion:'',
+    tipoComponenteSeleccionado:''
+  };
+  submitted = false;
+
+  codAplicacionDefault:string='1';
   transaccionSeleccionada:string='';
-  tipoComponenteSeleccionado:string='';
+  tipoComponente:string='';
+  listAplicaciones=[];
+  listaTransacciones=[];
   listaTipoComponentes=['STORE PROCEDURE', 'WEB SERVICE'];
 
   constructor(private supplierData:SupplierDataService,
-              private transaccionService:TransaccionService) { }
-  listAplicaciones=[];
-  listaTransacciones=[];
+    private transaccionService:TransaccionService) { }
+
+
   ngOnInit(): void {
     this.listAplicaciones=this.supplierData.listAplicaciones;
     this.listaTransacciones=this.transaccionService.listaTransacciones;
   }
-  onAgregarTrazabilidadInsumo(){
 
+  onAgregarTrazabilidadInsumo(){
+    this.submitted = true;
+   this.insumo.nombreAplicacion= this.signupForm.value.nombreAplicacion;
+   this.insumo.codigoProyecto= this.signupForm.value.codigoProyecto;
+   this.insumo.transaccion=this.signupForm.value.transaccion;
+   this.insumo.tipoComponenteSeleccionado=this.signupForm.value.tipoComponenteSeleccionado;
+   this.signupForm.reset();
   }
 }
