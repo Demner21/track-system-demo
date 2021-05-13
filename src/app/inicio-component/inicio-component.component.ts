66dc1927022a86fa840a6ccbe94fda59da2ca360
Demner <rlopezcr@everis.com>
2021-05-12 23:58:41 -0500

    correcciones basicas en vista principal

--- src/app/inicio-component/inicio-component.component.ts
+++ src/app/inicio-component/inicio-component.component.ts
@@ -13,5 +13,7 @@ export class InicioComponentComponent implements OnInit {
   }
 
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
-
+  imagesPath0 = '/assets/openphotonet_MAJ_8672.jpg';
+  imagesPath1 = '/assets/openphotonet_IMG_6831.JPG';
+  imagesPath2 = '/assets/2002_3_20_176_0_OPL.jpg';
 }
