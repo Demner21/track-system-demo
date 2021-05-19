344b2adf0e3e8320e661df6be235b28d15e05512
Demner <rlopezcr@everis.com>
2021-05-18 21:12:25 -0500

    agregando vista de listado de aplicaciones

--- src/app/aplicacion/listar-aplicaciones/listar-aplicaciones.component.ts
+++ src/app/aplicacion/listar-aplicaciones/listar-aplicaciones.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { from, interval, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { AplicacionService } from 'src/app/services/aplicacion.service';
 
 @Component({
   selector: 'app-listar-aplicaciones',
@@ -8,8 +9,8 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./listar-aplicaciones.component.css']
 })
 export class ListarAplicacionesComponent implements OnInit, OnDestroy {
-
-  constructor() { }
+  listaAplicacion:any;
+  constructor(private aplicacionService:AplicacionService) { }
   ngOnDestroy(): void {
     this.tempOnSubscription.unsubscribe();
   }
@@ -17,13 +18,27 @@ export class ListarAplicacionesComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     //const data = from(fetch('/api/endpoint'));
-    this.tempOnSubscription= interval(1000)
-      .pipe( map( (response: number)=>{ return 'Value of data: '+(response*10)}))
-      .subscribe({
-      next( response) {console.log(response);},
-      error(err) {console.log('Error: '+ err);},
-      complete(){console.log('Completed');}
-    });
+    // this.tempOnSubscription= interval(1000)
+    //   .pipe( map( (response: number)=>{ return 'Value of data: '+(response*10)}))
+    //   .subscribe({
+    //   next( response) {console.log(response);},
+    //   error(err) {console.log('Error: '+ err);},
+    //   complete(){console.log('Completed');}
+    // });
+
+    this.cargarListaAplicaciones();
+  }
+
+  private cargarListaAplicaciones() {
+    this.aplicacionService.getAll()
+                           .snapshotChanges()
+                           .pipe( map(changes => 
+                                changes.map(c => ({ key: c.payload.key, ...c.payload.val() })))
+    ).subscribe(
+      data => {
+        this.listaAplicacion = data;
+      }
+    );
   }
 
 }
