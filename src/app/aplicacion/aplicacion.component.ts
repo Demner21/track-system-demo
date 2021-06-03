544e4caa74d5838bb6f1ffacf37dacc4afdf2142
Demner <rlopezcr@everis.com>
2021-06-02 23:58:57 -0500

    mejora mensaje de creacion de aplicacion correcta

--- src/app/aplicacion/aplicacion.component.ts
+++ src/app/aplicacion/aplicacion.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit ,ViewChild} from '@angular/core';
-import { map } from 'rxjs/operators';
-import { NgForm, FormsModule } from '@angular/forms';
+import { Component, OnInit ,ViewChild } from '@angular/core';
+import { debounceTime } from 'rxjs/operators';
+import { NgForm } from '@angular/forms';
 import { AplicacionService } from '../services/aplicacion.service';
 import { Aplicacion } from '../model/aplicacion.model';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-aplicacion',
@@ -11,9 +12,16 @@ import { Aplicacion } from '../model/aplicacion.model';
 })
 export class AplicacionComponent implements OnInit {
   @ViewChild('form', { static: false }) signupForm: NgForm;
+
+  private _success = new Subject<string>();
   constructor(private aplicacionService:AplicacionService) { }
 
+
   ngOnInit(): void {
+    this._success.subscribe(message => this.successMessage = message);
+    this._success.pipe(debounceTime(3500)).subscribe(() => {
+      this.isLoadingSucces=false;
+    });
   }
 
   aplicacion={
@@ -33,13 +41,13 @@ export class AplicacionComponent implements OnInit {
     console.log(this.aplicacion);
     this.isLoading=false;
     this.isLoadingSucces=true;
-    this.successMessage="Aplicacion creada correctamente";
     this.signupForm.reset();
-
+    this._success.next("Aplicacion creada correctamente")
   }
 
   isLoading:boolean =false;
   isLoadingSucces:boolean =false;
   errorMessage: string=null;
   successMessage: string=null;
+
 }
